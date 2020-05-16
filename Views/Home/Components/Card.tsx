9699bfa9d54a6801836e98a0da2c94a1554bb5d7
Remy <insomnia128@hotmail.com>
2020-05-16 20:11:14 +0800

    Add card component for showing numbers

--- Views/Home/Components/Card.tsx
+++ Views/Home/Components/Card.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 interface Props {
-  isEmpty?: boolean
+  isEmpty?: boolean;
+  number?: number;
 }
 
 export default function Card(props: Props) {
-  if (props.isEmpty) {
+  if (props.isEmpty || !props.number) {
     return <View style={styles.emptyContainer}></View>
   }
   return (
     <View style={styles.container}>
-      <Text>20</Text>
+      <Text style={styles.text}>{props.number}</Text>
     </View>
   );
 }
@@ -20,10 +21,17 @@ export default function Card(props: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#121f75',
     height: 150,
     borderRadius: 20,
     margin: 10,
+    justifyContent: 'center'
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#e1e2e6',
+    fontWeight: '500'
   },
   emptyContainer: {
     flex: 1,
--- Views/Home/Index.tsx
+++ Views/Home/Index.tsx
@@ -1,15 +1,89 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
 import Card from "./Components/Card";
+import Header from "./Components/Header";
+import { NUMBER_OF_RANDOM_NUMBERS } from "../../Constants";
 
-export default class Home extends React.Component{
+interface Props {
+
+}
+interface State {
+  cardNumbers: number[];
+  steps: number;
+}
+export default class Home extends React.Component<Props, State>{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardNumbers: [],
+      steps: 309
+    };
+
+  }
+  componentDidMount() {
+    this.populateCards();
+  }
+
+  restart(self) {
+    self.setState({
+      steps: 0
+    });
+  }
+
+  populateCards() {
+    const randomNumbersMap = {};
+    const randomNumbers = [];
+    for (let i = 0; i < NUMBER_OF_RANDOM_NUMBERS; i++) {
+      let randomNumber = Math.ceil((Math.random() * 100) + 1);
+      while (randomNumbersMap[randomNumber]) {
+        randomNumber = Math.ceil((Math.random() * 100) + 1);
+      }
+      randomNumbersMap[randomNumber] = true;
+      randomNumbers.push(randomNumber);
+      randomNumbers.push(randomNumber);
+    }
+
+    this.randomizeArr(randomNumbers);
+    if (randomNumbers.length % 3 !== 0) {
+      while (randomNumbers.length % 3 !== 0) {
+        randomNumbers.push(-1);
+      }
+    }
+    this.setState({
+      cardNumbers: randomNumbers
+    });
+  }
+
+  randomizeArr(arr: number[]) {
+    for (let i = 0; i < arr.length; i++) {
+      const randomIndex = Math.ceil((Math.random() * i));
+      this.swap(arr, i, randomIndex);
+    }
+  }
+
+  swap(arr: number[], index1: number, index2: number) {
+    const temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Card/>
-        <Card/>
-        <Card isEmpty/>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Header restart={() => this.restart(this)} steps={this.state.steps} />
+        <FlatList
+          style={{ width: "100%" }}
+          numColumns={3}
+          data={this.state.cardNumbers}
+          renderItem={({ item }) => {
+            if (item === -1) {
+              return <Card isEmpty />
+            }
+            return <Card number={item} />
+          }}
+        />
+      </SafeAreaView>
     );
   }
 }
@@ -20,8 +94,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    display: "flex",
-    flexDirection: "row"
+    display: 'flex',
+    flexDirection: 'column'
   },
   card: {
     flex: 1,
